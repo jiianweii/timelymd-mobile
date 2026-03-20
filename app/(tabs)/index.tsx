f33a7681acf530983d91ac4a756dff6040727b4f
Jian Wei <itsjianweii@gmail.com>
2026-03-21 02:20:31 +0800

    feat: setup temp home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
-import { StyleSheet, Text } from "react-native";
+import CustomView from "@/components/common/CustomView";
+import ClinicCategories from "@/components/home/ClinicCategories";
+import Header from "@/components/home/Header";
 
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Test</Text>
-    </SafeAreaView>
+    <CustomView>
+      <Header />
+      <View style={styles.view}>
+        <ClinicCategories />
+      </View>
+    </CustomView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+  },
+});
