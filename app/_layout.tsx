f12468cda9fb8ac8b281af5eb7de5b8eb7192a6a
Jian Wei <itsjianweii@gmail.com>
2026-03-21 01:17:30 +0800

    feat: add custom fonts and fix statusbar style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,10 @@
+import {
+  MavenPro_400Regular,
+  MavenPro_500Medium,
+  MavenPro_600SemiBold,
+  MavenPro_700Bold,
+  useFonts,
+} from "@expo-google-fonts/maven-pro";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -8,6 +15,15 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    MavenPro_400Regular,
+    MavenPro_500Medium,
+    MavenPro_600SemiBold,
+    MavenPro_700Bold,
+  });
+
+  if (!loaded) return null;
+
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
@@ -17,7 +33,7 @@ export default function RootLayout() {
           options={{ presentation: "modal", title: "Modal" }}
         />
       </Stack>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
